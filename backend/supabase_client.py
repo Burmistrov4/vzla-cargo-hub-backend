80e98d749a9916abf81e33df050e337ca08362c6
Burmistrov4 <lorenzoroca333@gmail.com>
2026-04-28 01:07:25 -0400

    Intento de crear solo la conexion a supbase si las variables realmente existen

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -13,6 +13,15 @@ SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 if not SUPABASE_URL or not SUPABASE_PUBLISHABLE_KEY:
     print("⚠️ Aviso: Las credenciales de Supabase no se detectaron en el entorno.")
 
+# Solo creamos el cliente si las variables NO están vacías
+supabase_public = None
+if SUPABASE_URL and SUPABASE_PUBLISHABLE_KEY:
+    supabase_public = create_client(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY)
+
+supabase_admin = None
+if SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY:
+    supabase_admin = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+
 # Cliente para operaciones normales (el que usas en el 90% de la app)
 # Como RLS está desactivado, este cliente puede leer y escribir sin problemas.
 supabase_public: Client = create_client(
